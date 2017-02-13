15d312603bfb74c25dceb248c44bca89a21a578a
Graham Moss <graham.moss@hotmail.com>
2017-02-12 20:35:52 -0500

    format.csv block working

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,19 @@
 class BooksController < ApplicationController
 
   def index
+    @books = Book.all
+
     respond_to do |format|
       format.html
+      format.text
+      format.csv do
+        formated_csv = Book.generate_csv(@books)
+        render plain: formated_csv
+      end
     end
   end
+
+
 
 
 end
